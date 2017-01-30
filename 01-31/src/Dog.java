0e5e22195a0f2ebdc58a305a522b8162551ed1a4
Mujtaba Aslam <Mujtaba Aslam>
2017-01-30 17:24:29 -0600

    01-31

---
+++ 01-31/src/Dog.java
@@ -0,0 +1,23 @@
+
+public class Dog {
+    public String name;
+    public String breed;
+    public int age;
+    
+    public Dog(String name, String breed, int age)  {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    
+    public void birthday() {
+        this.age += 1;
+    }
+    
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
